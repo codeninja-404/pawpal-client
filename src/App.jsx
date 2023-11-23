afffeaffc3af83d1a97f5ee24016a4558686f042
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-11-24 01:13:18 +0600

    PawPal get a furry friend

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import logo from "./assets/logo.png";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo " alt="React logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
