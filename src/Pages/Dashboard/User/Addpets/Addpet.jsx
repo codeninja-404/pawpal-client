d23e2ce45b5faa1a0d1148c8af9ec6a660903715
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-11-28 03:24:14 +0600

    add pet form is ready

--- src/Pages/Dashboard/User/Addpets/Addpet.jsx
+++ src/Pages/Dashboard/User/Addpets/Addpet.jsx
@@ -33,7 +33,9 @@ const Addpet = () => {
   };
 
   const onSubmit = (values) => {
-    const pet = { ...values, image: imgURL };
+    const addedOn = new Date();
+    const adopted = false;
+    const pet = { ...values, image: imgURL, addedOn, adopted };
     console.log(pet);
 
     //  post data to database
@@ -44,6 +46,22 @@ const Addpet = () => {
 
     if (!values.name) {
       errors.name = "Name is required";
+    }
+
+    if (!values.age) {
+      errors.age = "Age is required";
+    }
+    if (!values.category) {
+      errors.category = "Category is required";
+    }
+    if (!values.location) {
+      errors.location = "Location is required";
+    }
+    if (!values.shortDescription) {
+      errors.shortDescription = "Short Description is required";
+    }
+    if (!values.longDescription) {
+      errors.longDescription = "Long Description is required";
     }
 
     return errors;
@@ -57,9 +75,9 @@ const Addpet = () => {
           onSubmit={onSubmit}
           validate={validate}
         >
-          <Form className=" mx-auto my-8 p-6 bg-white rounded-md shadow-md">
-            <div className="flex gap-2">
-              {/* <div className="mb-4">
+          <Form className=" mx-auto  p-6  rounded-md ">
+            <div className="md:flex gap-2 ">
+              <div className="mb-4 flex-1">
                 <label
                   htmlFor="image"
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -77,9 +95,9 @@ const Addpet = () => {
                   component="div"
                   className="text-red-500 text-sm"
                 />
-              </div> */}
+              </div>
 
-              <div className="mb-4">
+              <div className="mb-4 flex-1">
                 <label
                   htmlFor="name"
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -99,51 +117,53 @@ const Addpet = () => {
                 />
               </div>
             </div>
-            <div className="mb-4">
-              <label
-                htmlFor="age"
-                className="block text-gray-700 text-sm font-bold mb-2"
-              >
-                Age:
-              </label>
-              <Field
-                type="text"
-                id="age"
-                name="age"
-                className="w-full p-2 border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
+            <div className="md:flex gap-2">
+              <div className="mb-4 flex-1">
+                <label
+                  htmlFor="age"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Age:
+                </label>
+                <Field
+                  type="text"
+                  id="age"
+                  name="age"
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                />
+                <ErrorMessage
                   name="age"
                   component="div"
                   className="text-red-500 text-sm"
                 />
-            </div>
+              </div>
 
-            <div className="mb-4">
-              <label
-                htmlFor="category"
-                className="block text-gray-700 text-sm font-bold mb-2"
-              >
-                Category:
-              </label>
-              <Field
-                id="category"
-                name="category"
-                as="select"
-                className="w-full p-2 border border-gray-300 rounded-md"
-              >
-                <option value="dogs">Dogs</option>
-                <option value="birds">Birds</option>
-                <option value="fish">Fish</option>
-                <option value="cats">Cats</option>
-                <option value="mammals">Mammals</option>
-                <option value="reptiles">Reptiles</option>
-              </Field>
-              <ErrorMessage
+              <div className="mb-4 flex-1">
+                <label
+                  htmlFor="category"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Category:
+                </label>
+                <Field
+                  id="category"
+                  name="category"
+                  as="select"
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                >
+                  <option value="dogs">Dogs</option>
+                  <option value="birds">Birds</option>
+                  <option value="fish">Fish</option>
+                  <option value="cats">Cats</option>
+                  <option value="mammals">Mammals</option>
+                  <option value="reptiles">Reptiles</option>
+                </Field>
+                <ErrorMessage
                   name="category"
                   component="div"
                   className="text-red-500 text-sm"
                 />
+              </div>
             </div>
 
             <div className="mb-4">
@@ -160,10 +180,10 @@ const Addpet = () => {
                 className="w-full p-2 border border-gray-300 rounded-md"
               />
               <ErrorMessage
-                  name="location"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
+                name="location"
+                component="div"
+                className="text-red-500 text-sm"
+              />
             </div>
 
             <div className="mb-4">
@@ -174,17 +194,16 @@ const Addpet = () => {
                 Short Description:
               </label>
               <Field
-                as="text"
+                type="text"
                 id="shortDescription"
                 name="shortDescription"
                 className="w-full p-2 border border-gray-300 rounded-md"
               />
               <ErrorMessage
-              name="shortDescription"
-              component="div"
-              className="text-red-500 text-sm"
-            />
-              
+                name="shortDescription"
+                component="div"
+                className="text-red-500 text-sm"
+              />
             </div>
 
             <div className="mb-4">
@@ -201,10 +220,10 @@ const Addpet = () => {
                 className="w-full p-2 border border-gray-300 rounded-md"
               />
               <ErrorMessage
-                  name="longDescription"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
+                name="longDescription"
+                component="div"
+                className="text-red-500 text-sm"
+              />
             </div>
 
             <button
